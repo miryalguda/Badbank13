7f60dad9e69b06e90f80217e5ab74a20e81f9dc4
miryalguda <137521245+miryalguda@users.noreply.github.com>
2024-04-18 08:24:52 -0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -238,7 +238,7 @@ app.get("/gettrans", (req, res) => {
 
  mongoose.connect(process.env.MONGO_URI)
  .then( () => {
-     app.listen(process.env.PORT || 4000, () => {
+     app.listen(process.env.PORT || 8080, () => {
         console.log("Connected to DB and Server started on port 3260", process.env.PORT)
  })
 })
